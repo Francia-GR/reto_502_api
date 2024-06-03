81b7196e5126e7f90b5c5b9f2edf8a48e72cb570
fgarciaromero_i <fgarciaromero_i@didiglobal.com>
2024-06-03 01:30:05 -0600

    Updated pipeline

---
+++ notes_pipeline/pipeline.py
@@ -0,0 +1,9 @@
+'''
+1. Obtener Datos de la BD Fecha | ID | Ingreso  >> renombrar columnas
+2. Pivot Table por ID
+3. Hay fechas faltantes? Llenar fechas
+3.5 Checar valores atipicos
+4. Manejo de nulos (son al principio o no hubo ingresos)
+4. Agrupar por semana *
+5. 
+'''
